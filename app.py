6e251ac577154d9e764d3500abcb3634f1ab0b90
Gustavo Arjones <garjones@socialmetrix.com>
2017-01-11 19:54:54 -0300

    Initial Commit

---
+++ app.py
@@ -0,0 +1,73 @@
+from __future__ import print_function
+
+import docker
+from slackclient import SlackClient
+import os
+import sys
+import time
+import signal
+
+slack_token = None
+slack_channel = None
+
+APP_NAME = 'Docker Events Notifier'
+
+
+def get_config(env_key):
+    value = os.getenv(env_key)
+    if not value:
+        print('Environment variable {} is missing. Can\'t continue'.format(env_key))
+        sys.exit(1)
+    return value
+
+
+def watch_and_notify_events():
+    event_filters = {"event": "die"}
+
+    client = docker.DockerClient(base_url='unix://var/run/docker.sock')
+
+    for event in client.events(filters=event_filters, decode=True):
+        container_id = event['Actor']['ID'][:12]
+        attributes = event['Actor']['Attributes']
+        when = time.strftime('%Y-%m-%d %H:%M:%S %Z', time.localtime(event['time']))
+
+        message = ":rotating_light:   At _{}_ your container *{}* (_{}_) died. Image: *{}* Exit Code: *{}*" \
+            .format(when,
+                    attributes['name'],
+                    container_id,
+                    attributes['image'],
+                    attributes['exitCode'])
+
+        send_message(slack_channel, message)
+
+
+def send_message(channel, message):
+    global slack_token
+    global slack_channel
+
+    sc = SlackClient(slack_token)
+    sc.api_call(
+        "chat.postMessage",
+        channel=channel,
+        text=message
+    )
+    pass
+
+
+def exit_handler(_signo, _stack_frame):
+    send_message(slack_channel,
+                 ':disappointed: *{}* received *SIGTERM*. Goodbye!'.format(APP_NAME))
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    slack_token = get_config("SLACK_API_KEY")
+    slack_channel = get_config("SLACK_CHANNEL")
+
+    signal.signal(signal.SIGTERM, exit_handler)
+    signal.signal(signal.SIGINT, exit_handler)
+
+    send_message(slack_channel,
+                 ':bulb: *{}* reporting for duty! Alerts will be sent to this channel.'.format(APP_NAME))
+
+    watch_and_notify_events()
